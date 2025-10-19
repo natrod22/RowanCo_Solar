77bf8d7e0d185468da54b848c959721110e781f6
Natalie Rodriguez <33225820+natrod22@users.noreply.github.com>
2025-10-19 12:39:06 -0500

    Delete data directory

--- data/DOE_Electric_Power_Transmission_Lines_4.js
+++
@@ -1,1 +0,0 @@
-var json_DOE_Electric_Power_Transmission_Lines_4 = {"type":"FeatureCollection","name":"DOE_Electric_Power_Transmission_Lines_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
